bfe1fe0277d3d9825ba7686366639144ebea8cd1
angelesbg823 <angelesbarria823@gmail.com>
2022-04-05 20:46:55 -0400

    finished: busqueda.rb

--- busqueda.rb
+++ busqueda.rb
@@ -13,21 +13,37 @@ ventas = {
     Diciembre: 21000
 }
 
+#datos para ver que se vendió tal cantidad
 search_data = ARGV.map do |element|
     element.to_i
 end
 
-sales_inverted = ventas.invert
+#sales_inverted = ventas.invert
 
-search_data.each do |sale|
-#     #evaluar si la llave sale existe o es nil
-    if !sales_inverted[sale].nil?
-        puts sales_inverted[sale]
-    else
-        puts "no encontrado"
-    end 
-end
+# search_data.each do |sale|
+# #     #evaluar si la llave sale existe o es nil
+#     if !sales_inverted[sale].nil? #nil? es opcional
+#         puts sales_inverted[sale]
+#     else
+#         puts "no encontrado"
+#     end 
+# end
 
 # print sales_inverted[15000]
 # print sales_inverted[31000].nil? 
 # print sales_inverted[27000].nil? 
+
+founded = false #falso porque no se ha encontrado nada
+
+search_data.each do |sale|
+    ventas.each do |key, value| #va a recorre el hash uno por uno 
+        if sale == value 
+            puts key #llave vendría a ser el mes
+            founded = true #true porque la encontró
+        end
+    end
+    if founded == false
+        puts "no encontrado"
+    end
+    founded = false
+end
